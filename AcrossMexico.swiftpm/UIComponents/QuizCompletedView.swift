a8c48d8bece0768b2fc994efb876a508c2643bd0
Mariano Barberi Garza <93608793+Bdelas777@users.noreply.github.com>
2024-01-28 11:48:50 -0600

    Listo

--- AcrossMexico.swiftpm/UIComponents/QuizCompletedView.swift
+++ AcrossMexico.swiftpm/UIComponents/QuizCompletedView.swift
@@ -21,32 +21,34 @@ struct QuizCompletedView: View {
                          "THAT'S A WRAP" :
                             "GAME OVER",
                          size: 30)
-                .padding()
+            .padding()
             
             ReusableText(text: gameManagerVM.model.quizWinningStatus
                          ? "Thank you for playing!!"
                          : "Better luck next time",
                          size: 30)
-                .padding()
-            
-            Button {
-                gameManagerVM.restartGame()
-            } label: {
-                HStack {
-                    Image(systemName: "play.fill")
-                        .foregroundColor(.white)
-                        .font(.system(size: 24))
-                        .padding()
-                    
-                    Text("Play Again")
-                        .foregroundColor(.white)
-                        .font(.system(size: 24, weight: .bold, design: .rounded))
-                }
-            }.frame(width: 300, height: 60, alignment: .center)
-                .background(.purple.opacity(0.7))
-                .cornerRadius(30)
-                .padding()
-            
+            .padding()
+            if !gameManagerVM.model.quizWinningStatus {
+                
+                Button {
+                    gameManagerVM.restartGame()
+                } label: {
+                    HStack {
+                        Image(systemName: "play.fill")
+                            .foregroundColor(.white)
+                            .font(.system(size: 24))
+                            .padding()
+                        
+                        Text("Play Again")
+                            .foregroundColor(.white)
+                            .font(.system(size: 24, weight: .bold, design: .rounded))
+                    }
+                }.frame(width: 300, height: 60, alignment: .center)
+                    .background(.purple.opacity(0.7))
+                    .cornerRadius(30)
+                    .padding()
+                
+            }
         }
     }
 }
