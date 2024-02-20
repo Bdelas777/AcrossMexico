32f0fb3913fd216c4247c774d070fb1d42f78150
Mariano Barberi Garza <93608793+Bdelas777@users.noreply.github.com>
2024-02-19 21:01:58 -0600

    prueba

--- AcrossMexico.swiftpm/Scenes/RealityScene.swift
+++ AcrossMexico.swiftpm/Scenes/RealityScene.swift
@@ -11,45 +11,49 @@ import RealityKit
 
 struct RealityScene: View {
     var body: some View {
-        ARViewContainer()
-            .edgesIgnoringSafeArea(.all)
+        AsyncARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct AsyncARViewContainer: View {
+    @State private var arViewLoaded = false
+    
+    var body: some View {
+        if arViewLoaded {
+            ARViewContainer()
+        } else {
+            ProgressView() // Mostrar un indicador de progreso mientras se carga
+                .onAppear {
+                    // Simular una carga asíncrona
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        arViewLoaded = true
+                    }
+                }
+        }
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
-    @State private var isLoading = true
-
     func makeUIView(context: Context) -> ARView {
         let arView = ARView(frame: .zero)
         arView.cameraMode = .ar
         
-        // Load the model asynchronously
-        loadModel(on: arView)
-
+        // Load the "Regalo" scene from the "Experience" Reality File
+        let scene = try! Entity.load(named: "Regalo")
+        
+        let boxAnchor = AnchorEntity(.plane(
+            [ .horizontal],
+            classification: [.floor],
+            minimumBounds: [1.0, 1.0]
+        ))
+        
+        boxAnchor.addChild(scene)
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
         return arView
     }
-
+    
     func updateUIView(_ uiView: ARView, context: Context) {}
-
-    func loadModel(on arView: ARView) {
-        // Simulate async model loading
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            // Once the model is loaded, set isLoading to false
-            self.isLoading = false
-
-            // Load the "Regalo" scene from the "Regalo" Reality File
-            if let scene = try? Entity.load(named: "Regalo") {
-                let boxAnchor = AnchorEntity(.plane(
-                    [.horizontal],
-                    classification: [.floor],
-                    minimumBounds: [1.0, 1.0]
-                ))
-
-                boxAnchor.addChild(scene)
-
-                // Add the box anchor to the scene
-                arView.scene.anchors.append(boxAnchor)
-            }
-        }
-    }
 }
